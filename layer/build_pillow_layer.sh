c61f22d06b8dc12877f9eda10658a44cff85313c
CCao666 <chunyacao2@gmail.com>
2026-03-03 21:46:07 -0500

    Update build_pillow_layer.sh

--- layer/build_pillow_layer.sh
+++ layer/build_pillow_layer.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
 
+set -e
+
+echo "Building Pillow Lambda Layer..."
+
+rm -rf python pillow-layer.zip
+mkdir python
+
+docker run --rm --platform linux/arm64 \
+  --entrypoint /bin/bash \
+  -v "$PWD":/var/task \
+  public.ecr.aws/lambda/python:3.12 \
+  -lc "pip install pillow -t python/"
+
+zip -r pillow-layer.zip python
+
+echo "Layer build complete: pillow-layer.zip"
